7cddff370c78857e55ff473483ffcdfaf87ea6a8
ritu2005425 <66471042+ritu2005425@users.noreply.github.com>
2020-06-05 16:41:23 +0530

    Create biggestnumber.c

---
+++ biggestnumber.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a[6],i,n;
+   int *p;
+   p=&a;
+   printf("enter elemet");
+   for(i=0;i<6;i++)
+   {
+
+       scanf("%d",&a[i]);
+
+   }
+   n=*p;
+   for(i=0;i<6;i++)
+   {
+
+       if(a[i]>*p)
+       {
+           int temp;
+
+           temp=a[i];
+          a[i]=*p;
+          *p=temp;
+
+       }
+
+    }
+printf("%d",*p);
+return 0;
+}
+
